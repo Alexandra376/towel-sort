3c1fb46ba452d8a63a272b2d0b9ea90cf3200916
Alexandra <alexandra@192.168.1.50>
2023-02-15 21:07:58 +0300

    refactor: apply prettier

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort (matrix) { 
+  if (matrix === undefined) {
+    return []
+  }
+  matrix.filter((elem,index) => {
+    if (index % 2 !== 0) {
+      elem.reverse();
+    }
+  })
+  return matrix.flat()
+};
+
+
+
+
+
+
+
